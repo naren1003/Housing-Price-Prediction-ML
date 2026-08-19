c2107242504fb590c15b619e71c4d2a524aa6684
NARENDRA G S <narenskills@gmail.com>
2026-08-19 22:20:37 +0530

    check

--- data.py
+++ data.py
@@ -3,4 +3,4 @@ import kagglehub
 # Download latest version
 path = kagglehub.dataset_download("vikrishnan/boston-house-prices")
 
-print("Path to dataset files:", path)
+print("Path to dataset files:", path) 
